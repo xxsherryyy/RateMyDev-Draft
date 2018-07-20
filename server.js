08c869239350a85f3b2472141a1da2916a4319c6
Paul Rose <prose2334@gmail.com>
2018-07-19 20:01:31 -0500

    test react add junk code and some server code

---
+++ server.js
@@ -0,0 +1,63 @@
+// Required Dependencies
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require("mongoose");
+const passport = require('passport');
+const expressSession = require('express-session');
+const PORT = process.env.PORT || 3000;
+
+// Express server
+const app = express();
+
+// const logger = require("morgan");
+// const path = require("path");
+
+// Use body-parser for handling form submissions
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Use express.static to serve the public folder as a static directory
+// app.use(express.static("public"));
+// Serve up static assets
+app.use(express.static("client/build"));
+// Add routes, both API and view
+app.use(routes);
+
+
+// Use morgan logger for logging requests
+// app.use(logger("dev"));
+
+// // Show mongoose errors
+// db.on("error", function(error) {
+//     console.log("Mongoose Error: ", error);
+// });
+
+// // Log a success message
+// db.once("open", function() {
+//     console.log("Mongoose connection successful");
+// });
+
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+
+
+// Configuring Passport
+//Middleware 
+
+
+// Initialize Passport
+
+
+// Connect routes
+
+
+// var db = mongoose.connection;
+
+// Start the server
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!");
+  });
--- src/App.js
+++ src/App.js
@@ -5,15 +5,23 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <div class="jumbotron jumbotron-fluid col-md-6 offset-md-3">
+          <div class="container">
+            <h1 class="display-4">RATE THAT DEV</h1>
+            <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+          </div>
+        </div>
       </div>
+      // <div className="App">
+      //   <header className="App-header">
+      //     <img src={logo} className="App-logo" alt="logo" />
+      //     <h1 className="App-title">Welcome to React</h1>
+      //   </header>
+      //   <p className="App-intro">
+      //     To get started, edit <code>src/App.js</code> and save to reload.
+      //   </p>
+      // </div>
     );
   }
 }
